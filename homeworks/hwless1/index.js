e52931aba87e586d00def09d7cd6f0acc5820d1c
“tohankov” <tohankov@gmail.com>
2023-12-01 23:29:35 +0200

    hw1

--- homeworks/hwless1/index.js
+++ homeworks/hwless1/index.js
@@ -10,6 +10,7 @@ const age = prompt('Напишіть рік Вашого народження');
 //Рахуємо вік народження
 console.log(year - age);
 
+//Вводимо кількість долл з округленням(опанували масфлор)
 const bank = prompt('Введіть кількість $')
 const euro = 1.1;
 const count = Math.floor(bank * euro);
